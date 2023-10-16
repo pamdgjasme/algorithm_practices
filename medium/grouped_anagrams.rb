e413d8dd16baf9e0d752eab12ffa73ceaa0612c3
Pam Jasme <dhoreenpamjasme@gmail.com>
2023-10-16 23:25:09 +0800

    Medium: Grouped Anagram (Revision)

--- medium/grouped_anagrams.rb
+++ medium/grouped_anagrams.rb
@@ -1,17 +1,16 @@
 # @param {String[]} strs
 # @return {String[][]}
-# Given an array of strings strs, group the anagrams together. You can return the answer in any order.
-# Input: strs = ["eat","tea","tan","ate","nat","bat"]
-# Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
-
 def group_anagrams(strs)
     grouped_anagrams = {}
     strs.each do |str|
-        str_bytes = str.bytes.sum
-        grouped_anagrams[str_bytes] = [] unless grouped_anagrams.key?(str_bytes)
+        sorted_str = str.chars.sort
+        grouped_anagrams[sorted_str] = [] unless grouped_anagrams.key?(sorted_str)
 
-        grouped_anagrams[str_bytes] << str
+        if sorted_str === str.chars.sort
+            grouped_anagrams[sorted_str] << str
+        else
+            grouped_anagrams[str] = str
+        end
     end
-
     grouped_anagrams.values
 end
