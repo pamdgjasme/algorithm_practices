d295be0a4b8363b94ef9508f783a83c7d95da6e5
Pam Jasme <dhoreenpamjasme@gmail.com>
2023-10-23 09:46:22 +0800

    Medium: Valid Sudoku (Revisions)

--- medium/valid_sudoku.rb
+++ medium/valid_sudoku.rb
@@ -6,70 +6,47 @@ QUADRANT_GROUP_BY = 9
 
 def is_valid_sudoku(board)
     [
-        valid_rows?(board),
-        valid_cols?(board),
+        valid_rows_and_cols?(board),
         valid_quadrant?(board)
     ].all?
 end
 
-def valid_rows?(board)
-    valid_flag = true
-    board.each do |row|
-        unless valid_array?(row)
-            valid_flag = false
-            break
-        end
-    end
-    valid_flag
-end
-
-def valid_cols?(board)
-    valid_flag = true
+def valid_rows_and_cols?(board)
     iteration_counter = (0..board.length - 1)
-    iteration_counter.each do |column_index|
-        column = []
-        iteration_counter.each do |row_index|
-            column << board[row_index][column_index]
-        end
-        unless valid_array?(column)
-            valid_flag = false
-            break
-        end
-    end
-    valid_flag
-end
+    iteration_counter.each do |row_idx|
+        temp_rows = []
+        temp_cols = []
+        iteration_counter.each do |col_idx|
+            row_value = board[col_idx][row_idx]
+            col_value = board[row_idx][col_idx]
 
+            temp_rows << row_value if row_value != "."
+            temp_cols << col_value if col_value != "."
+        end
+        return false if !valid_array?(temp_rows) || !valid_array?(temp_cols)
+    end
+    true
+end
 
 def valid_array?(arr)
     arr_temp = []
-    valid    = true
     arr.each do |value|
-        value = value.to_i
-        next if value == 0
+        next if value == "."
+        return false if arr_temp.include?(value)
 
-        if arr_temp.include?(value)
-            valid = false
-            break
-        else
-            arr_temp << value
-        end
+        arr_temp << value
     end
-
-    valid
+    true
 end
 
 def valid_quadrant?(board)
     quadrants = generate_quadrant(board)
-    valid_flag = true
     quadrants.each do |quadrant|
         quadrant.each_slice(QUADRANT_GROUP_BY) do |grouped|
-            unless valid_array?(grouped)
-                valid_flag = false
-                break
-            end
+            return false unless valid_array?(grouped)
         end
     end
-    valid_flag
+    true
 end
 
 def generate_quadrant(board)
@@ -79,6 +56,5 @@ def generate_quadrant(board)
             quadrants[index] = quadrants[index] + grouped
         end
     end
-
     quadrants
 end
